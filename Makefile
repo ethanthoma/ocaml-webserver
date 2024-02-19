86991876c477e63ebe6d9d9e4a6ad4d70eac27ae
Ethan Thoma <ethanthoma@gmail.com>
2024-02-19 01:28:04 -0800

    Updated flake to make docker image, makefile for ease

---
+++ Makefile
@@ -0,0 +1,21 @@
+.PHONY: run
+
+IMAGE_NAME = webserver
+TAG = latest
+CONTAINER_NAME = "webserver-instance"
+LOCAL_HOST = 127.0.0.1:3000
+
+build:
+	nix build .#docker
+
+run: build
+	docker load < result
+	docker run -d -t --name $(CONTAINER_NAME) -p $(LOCAL_HOST):3000 $(IMAGE_NAME):$(TAG) 
+
+healthy:
+	wget $(LOCAL_HOST)/healthy
+
+clean:
+	-docker stop $(CONTAINER_NAME)
+	-docker rm $(CONTAINER_NAME)
+	-docker rmi $(IMAGE_NAME):$(TAG)
--- flake.nix
+++ flake.nix
@@ -8,13 +8,14 @@
     outputs = { self, flake-utils, opam-nix, nixpkgs }:
     let 
         package = "webserver";
+        src = ./.;
     in flake-utils.lib.eachDefaultSystem (system:
         let
             pkgs = nixpkgs.legacyPackages.${system};
 
             on = opam-nix.lib.${system};
 
-            scope = on.buildDuneProject { } package ./. { 
+            scope = on.buildDuneProject { } package src { 
                 ocaml-base-compiler = "*"; 
                 opium = "*";
                 reason = "*";
@@ -33,15 +34,54 @@
                         final.easy_logging
                         final.fuzzy_match
                         final.omd
-
-                        pkgs.bash
                     ];
                     buildPhase = "dune build --release"; 
+                    postInstall = ''
+                        cp -rf $src/public $out/public
+                        cp -rf $src/blogs $out/blogs
+                    '';
                 });
             };
-        in {
+
             legacyPackages = scope.overrideScope' overlay;
-            packages.default = self.legacyPackages.${system}.${package};
+
+            default = self.legacyPackages.${system}.${package};
+
+            image = pkgs.dockerTools.buildImage {
+                name = "webserver";
+                tag = "latest";
+                created = "now";
+                fromImage = pkgs.dockerTools.pullImage {
+                    imageName = "alpine";
+                    imageDigest = "sha256:c5b1261d6d3e43071626931fc004f70149baeba2c8ec672bd4f27761f8e1ad6b";
+                    sha256 = "0h4k892b3izv6ypk3w07z96asmpr987cmbcianajhdk0kz4z62my";
+                    finalImageName = "alpine";
+                    finalImageTag = "3.19.1";
+                };
+                copyToRoot = pkgs.buildEnv {
+                    name = "webserver";
+                    paths = [
+                        default
+                        pkgs.coreutils
+                    ];
+                    pathsToLink = [ "/bin" "/public" "/blogs" ];
+                };
+                config = {
+                    Cmd = [ 
+                        "${default}/bin/${package}"
+                    ];
+                    ExposedPorts = {
+                        "3000/tcp" = {};
+                    };
+                };
+            };
+        in {
+            inherit legacyPackages;
+            packages = {
+                inherit default;
+                docker = image;
+            };
+            defaultPackage = default;
         }
     );
 }
