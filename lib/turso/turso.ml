65463fa358a3c602becbaf82ac3299bd063dd5f1
Ethan Thoma <ethanthoma@gmail.com>
2024-03-04 23:24:37 -0800

    fixed missing lib

--- lib/turso/turso.ml
+++ lib/turso/turso.ml
@@ -1,18 +1,10 @@
-let json_payload =
-    `Assoc [
-        ("requests", `List [
-            `Assoc [("type", `String "execute"); ("stmt", `Assoc [("sql", `String "SELECT * FROM blogs")])];
-            `Assoc [("type", `String "close")]
-        ])
-    ] 
-
 open Ctypes
 open Foreign
 
 let lib = Dl.dlopen ~filename:"_build/default/lib/turso/dllturso.so"
             ~flags:Dl.[RTLD_NOW; RTLD_GLOBAL]
 
-let () = 
+let () =
     let init = foreign "Init" (void @-> returning void) ~from:lib in
     init ()
 
