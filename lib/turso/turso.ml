6e7d99f677f04e03d695fda72e156064282d6cd4
Ethan Thoma <ethanthoma@gmail.com>
2024-03-07 17:50:12 -0800

    simplified blog.re, added date, refactored blog parsing from go

--- lib/turso/turso.ml
+++ lib/turso/turso.ml
@@ -15,33 +15,25 @@ type blog = {
     date: string;
 }
 
-let get_blogs unit = 
-    let query_blogs = foreign "GetBlogTable" (void @-> returning string) in
-    let json = Yojson.Basic.from_string @@ query_blogs unit in
-    let open Yojson.Basic.Util in
-    List.map
-        (fun json -> begin
-            let title = json |> member "Title" |> to_string in
-            let filename = json |> member "Slug" |> to_string in
-            let description = json |> member "Description" |> to_string in 
-            let content = json |> member "Content" |> to_string in
-            let date = json |> member "Date" |> to_string in
-            let tags = json |> member "Tags" |> to_list |> filter_string in
-            { filename; title; description; content; tags; date; }
-            end
-        )
-        (json |> to_list )
-;;
-
-let get_blog_by_slug slug =
-    let query_blog_by_slug = foreign "GetBlogBySlug" (string @-> returning string) in
-    let json = Yojson.Basic.from_string @@ query_blog_by_slug slug in
+let json_to_blog json =
     let open Yojson.Basic.Util in
     let title = json |> member "Title" |> to_string in
     let filename = json |> member "Slug" |> to_string in
     let description = json |> member "Description" |> to_string in 
     let content = json |> member "Content" |> to_string in
-    let date = json |> member "Date" |> to_string in
+    let date = String.sub (json |> member "Date" |> to_string) 0 10 in
     let tags = json |> member "Tags" |> to_list |> filter_string in
     { filename; title; description; content; tags; date; }
+;;
+
+let get_blogs unit = 
+    let query_blogs = foreign "GetBlogTable" (void @-> returning string) in
+    let json = Yojson.Basic.from_string @@ query_blogs unit in
+    List.map json_to_blog (json |> Yojson.Basic.Util.to_list )
+;;
+
+let get_blog_by_slug slug =
+    let query_blog_by_slug = foreign "GetBlogBySlug" (string @-> returning string) in
+    let json = Yojson.Basic.from_string @@ query_blog_by_slug slug in
+    json_to_blog json
 ;;
--- src/components/blog.re
+++ src/components/blog.re
@@ -1,22 +1,13 @@
 open Tyxml;
 
-let component = (blog: Turso.blog) => {
-    print_endline(blog.content);
-    let filename = "./blogs/" ++ blog.filename;
+let component = (blog: Turso.blog) => 
+    <article className="blog-content">
+        <div className="date">{Html.txt(blog.date)}</div>
+        {
+            blog.content
+            |> Omd.of_string 
+            |> Omd.to_html
+            |> Html.Unsafe.data
+        }
+    </article>
 
-    let file_data =
-        In_channel.input_all
-        |> In_channel.with_open_bin(filename)
-    ;
-
-    print_endline(file_data);
-
-    let blog_content = 
-        blog.content
-        |> Omd.of_string 
-        |> Omd.to_html
-    ;
-
-    "<article class=\"blog-content\">" ++ blog_content ++ "</article>"
-    |> Html.Unsafe.data;
-}
